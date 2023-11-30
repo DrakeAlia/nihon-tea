7982faa7909f86964ec1d115ae2e548adb9f3c5a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-11-30 15:05:11 -0800

    create simple header and text, created components, ui, and lib folders

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex"></div>
-    </main>
+    <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+      <h1 className="text-4xl font-bold text-yellow-500 sm:text-6xl">
+        Nihon Che Essence
+      </h1>
+      <p className="mt-6 text-lg max-w-prose text-muted-forground">
+        Crafting Tranquility Through Authentic Japanese Tea
+      </p>
+      <p className="mt-3 text-lg max-w-prose text-muted-forground">
+        Discover the art of tea-making, immerse yourself in mindful rituals, and
+        savor the serenity of Nihon Cha Essence – where every cup is an ode to
+        the soul-soothing essence of Japanese tea.
+      </p>
+    </div>
   );
 }
